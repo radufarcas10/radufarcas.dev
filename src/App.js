dd21a758c5e53cf44ab5e6b7fc2b5d66373c573a
radufarcas10 <f.radu10@yahoo.com>
2021-02-20 15:38:06 +0200

    start with create-react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>About Radu Farcas</code>
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
